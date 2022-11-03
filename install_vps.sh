d465fb99a739e5a672abd46fe46da4dc98791840
Tạ Quốc Cường <63773326+taquoccuong2002@users.noreply.github.com>
2022-11-04 00:57:54 +0700

    Update install_vps.sh

--- install_vps.sh
+++ install_vps.sh
@@ -1,3 +1,3 @@
-apt install docker.io
+snap install docker
 docker pull traffmonetizer/cli:latest
 docker run -i --name tm traffmonetizer/cli start accept --token rcX2TPe6GS0GDTR40LxS/9sEcn6WCz1W7beRjUPr1gE=
